5fcaa285313d060e1c9cea796822b9d7e1a43e95
tanakatakamasa-l <74941080+tanakatakamasa-l@users.noreply.github.com>
2021-09-13 09:37:38 +0900

    アニメグラム作成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = '5e2884e6056d9230c509e32fc697c34fcf53f88bcc7f382078e449d0151bcfce05e21523786e5140425efbcb96a2013df9bebd3988b5de7437c6bf1cbdf1c43e'
